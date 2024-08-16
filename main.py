326a2841d812a800eac5a80fb3974e7a574cd4a2
Gabriel Cunha Nalim <20211IMI006@cefetes.br>
2024-08-16 12:03:12 -0300

    novo visual

--- main.py
+++ main.py
@@ -1,17 +1,27 @@
-from fastapi import Depends, FastAPI
+# from fastapi import Depends, FastAPI
+# from fastapi.staticfiles import StaticFiles
+# import uvicorn
+# from repositories.usuario_repo import UsuarioRepo
+# from routes import main_routes
+# from util.auth import checar_permissao, middleware_autenticacao
+# from util.exceptions import configurar_excecoes
+
+# UsuarioRepo.criar_tabela()
+# app = FastAPI(dependencies=[Depends(checar_permissao)])
+# app.mount(path="/static", app=StaticFiles(directory="static"), name="static")
+# app.middleware(middleware_type="http")(middleware_autenticacao)
+# configurar_excecoes(app)
+# app.include_router(main_routes.router)
+
+# if __name__ == "__main__":
+#     uvicorn.run(app="main:app", port=8000, reload=True)
+
+from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
-import uvicorn
-from repositories.usuario_repo import UsuarioRepo
-from routes import main_routes
-from util.auth import checar_permissao, middleware_autenticacao
-from util.exceptions import configurar_excecoes
+from routes.main_routes import router as main_router
 
-UsuarioRepo.criar_tabela()
-app = FastAPI(dependencies=[Depends(checar_permissao)])
+app = FastAPI()
+
 app.mount(path="/static", app=StaticFiles(directory="static"), name="static")
-app.middleware(middleware_type="http")(middleware_autenticacao)
-configurar_excecoes(app)
-app.include_router(main_routes.router)
 
-if __name__ == "__main__":
-    uvicorn.run(app="main:app", port=8000, reload=True)
+app.include_router(main_router)
--- routes/main_routes.py
+++ routes/main_routes.py
@@ -1,24 +1,10 @@
 from fastapi import APIRouter, Request
-from fastapi.responses import HTMLResponse
-
-from util.templates import obter_jinja_templates
+from fastapi.templating import Jinja2Templates
 
 router = APIRouter()
-templates = obter_jinja_templates("templates/main")
 
+templates =Jinja2Templates(directory="templates")
 
-@router.get("/", response_class=HTMLResponse)
+@router.get("/")
 async def get_root(request: Request):
-    return templates.TemplateResponse("pages/inicio.html", {"request": request})
-
-@router.get("/confEmail", response_class=HTMLResponse)
-async def get_root(request: Request):
-    return templates.TemplateResponse("pages/confEmail.html", {"request": request})
-
-@router.get("/faq", response_class=HTMLResponse)
-async def get_root(request: Request):
-    return templates.TemplateResponse("pages/faq.html", {"request": request})
-
-@router.get("/senha", response_class=HTMLResponse)
-async def get_root(request: Request):
-    return templates.TemplateResponse("pages/senha.html", {"request": request})
+    return templates.TemplateResponse("pages/index.html", {"request":request})
