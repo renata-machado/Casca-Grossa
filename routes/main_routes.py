e9662c1a91925d5394620ed893e67a90745526e0
Renata <renatamachadobarreto@gmail.com>
2024-09-16 18:29:48 -0300

    form cadastro

--- routes/main_routes.py
+++ routes/main_routes.py
@@ -8,3 +8,12 @@ templates =Jinja2Templates(directory="templates")
 @router.get("/")
 async def get_root(request: Request):
     return templates.TemplateResponse("pages/tela_inicial.html", {"request":request})
+@router.get("/entrar")
+async def get_root(request: Request):
+    return templates.TemplateResponse("pages/Entre.html", {"request":request})
+@router.get("/cadastrar")
+async def get_root(request: Request):
+    return templates.TemplateResponse("pages/Cadastro.html", {"request":request})
+@router.get("/form_cadastro")
+async def get_root(request: Request):
+    return templates.TemplateResponse("pages/form_cadastro.html", {"request":request})
