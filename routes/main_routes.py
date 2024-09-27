aaa6c0ec11d2c410a5415acdef1c7787c30903dd
gabrielCunha21 <gabrielcumhanalim@gmail.com>
2024-09-27 07:30:12 -0300

    alteração de rota principal

--- routes/main_routes.py
+++ routes/main_routes.py
@@ -7,15 +7,15 @@ templates =Jinja2Templates(directory="templates")
 
 @router.get("/")
 async def get_root(request: Request):
-    return templates.TemplateResponse("pages/tela_inicial.html", {"request":request})
+    return templates.TemplateResponse("pages/Entre.html", {"request":request})
 
 @router.get("/cadastro")
 async def get_cadastro(request: Request):
     return templates.TemplateResponse("pages/Cadastro.html", {"request":request})
 
-@router.get("/entre")
-async def get_entre(request: Request):
-    return templates.TemplateResponse("pages/Entre.html", {"request":request})
+@router.get("/PaginaPrincipal")
+async def get_PPRINCIPAL(request: Request):
+    return templates.TemplateResponse("pages/tela_inicial.html", {"request":request})
 
 @router.get("/compra")
 async def get_telaCompra(request: Request):
